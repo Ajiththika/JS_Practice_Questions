5532d8093be9a59cf370c289a9dfa1cc086e9045
Ajiththika <ajiththika17@gmail.com>
2025-11-19 12:24:27 +0530

    Exercise update

--- Date, Null, Undefined, BigInt, Symbol.js
+++ Date, Null, Undefined, BigInt, Symbol.js
@@ -1,12 +1,23 @@
 // Display current date and time using your method
 const today = new Date();
-console.log("Date (toDateString):", today.toDateString());
-console.log("Date (toLocaleDateString):", today.toLocaleDateString()); 
-console.log("Time (toLocaleTimeString):", today.toLocaleTimeString()); 
+console.log(today.toDateString());
+console.log(today.toLocaleDateString()); 
+console.log(today.toLocaleTimeString()); 
 
 // Calculate days until New Year
 const newYear = new Date(today.getFullYear() + 1, 0, 1);
 const diffTime = newYear - today;
-const remainingDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+const remainingDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
 
 console.log("Days until New Year:", remainingDays);
+
+let formattedDate = new Date().toLocaleString("en-US", {
+  hour12: false,
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+});
+
